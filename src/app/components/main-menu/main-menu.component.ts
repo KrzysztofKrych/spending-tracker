e1768e91736d5145b9ae0fef4508b64c4c2d8df2
krzysztof <krzysztofkrych2@gmail.com>
2020-06-21 12:44:37 +0200

    codeclean

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AppState } from 'src/app/app.state';
-import { Spending } from 'src/app/modules/spending.module';
-import { Store } from '@ngrx/store';
-import { AddIncome } from 'src/app/actions/spending.actions';
 
 @Component({
   selector: 'app-main-menu',
@@ -10,7 +6,7 @@ import { AddIncome } from 'src/app/actions/spending.actions';
   styleUrls: ['./main-menu.component.scss']
 })
 export class MainMenuComponent implements OnInit {
-  menu = [{name: "Spending"}, {name: "Transaction"}, {name: "Categories"}, {name: "Accounts"}]
+  menu = [{name: "Spending"}, {name: "Transaction"}, {name: "Categories"}, {name: "Accounts"}];
   ngOnInit(): void {
   }
 
