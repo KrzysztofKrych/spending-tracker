f48e2b74d62e68194c75696e871ca5f23f643f9e
krzysztof <krzysztofkrych2@gmail.com>
2020-06-22 18:10:54 +0200

    moved to componnets folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
-import { TransactionsComponent } from './transactions/transactions.component';
-import { SpendingComponent } from './spending/spending.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { AccountsComponent } from './accounts/accounts.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { SpendingComponent } from './components/spending/spending.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { MainPageComponent } from './components/main-page/main-page.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
 import { StoreModule } from '@ngrx/store';
 import spendingReducer from './reducers/spending.reducer';
-import { TransactionsComponent } from './transactions/transactions.component';
-import { SpendingComponent } from './spending/spending.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { AccountsComponent } from './accounts/accounts.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { SpendingComponent } from './components/spending/spending.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 @NgModule({
   declarations: [
